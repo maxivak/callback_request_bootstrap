195ce17ceee80b1efac6c596835ed5f5c63f1c36
Max Ivak <maxivak@gmail.com>
2015-04-03 21:16:34 +0300

    generator - install

--- lib/generators/callback_request_bootstrap/install/install_generator.rb
+++ lib/generators/callback_request_bootstrap/install/install_generator.rb
@@ -1,11 +1,13 @@
 require 'rails/generators/active_record'
-module ActiveAdmin
+require 'rails/generators/migration'
+
+module CallbackRequestBootstrap
   module Generators
     class InstallGenerator < ActiveRecord::Generators::Base
-      desc "Installs callback_request_boostrap and generates the necessary migrations"
+      desc "Installs callback_request_bootstrap and generates the necessary migrations"
       source_root File.expand_path("../templates", __FILE__)
 
-      def copy_migrations
+      def create_migrations
         migration_template 'migrations/create_requests.rb', 'db/migrate/create_requests.rb'
       end
 
